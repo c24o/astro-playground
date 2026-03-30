7fa9ed94df10801cdc01e2dff467448ab891716f
Carlos Guzman <caralbgm@gmail.com>
2026-03-29 20:52:36 -0500

    Add RSS feed.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://quiet-croquembouche-1d5bbe.netlify.app/"
+});
